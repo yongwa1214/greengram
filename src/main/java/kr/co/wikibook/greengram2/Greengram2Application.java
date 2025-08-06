2cab8dc6feeee9cdd5969aed199e84d80cb90df6
yongwa1214 <yongwa1214@gmail.com>
2025-08-06 09:59:22 +0900

    초기 세팅

---
+++ src/main/java/kr/co/wikibook/greengram2/Greengram2Application.java
@@ -0,0 +1,13 @@
+package kr.co.wikibook.greengram2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Greengram2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Greengram2Application.class, args);
+    }
+
+}
---
+++ src/test/java/kr/co/wikibook/greengram2/Greengram2ApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.co.wikibook.greengram2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Greengram2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
